67c3c4749fc905974d182943f1b7a37432a51f66
codeecoffee <fellipefl7@msn.com>
2020-06-18 16:01:56 -0300

    fixed bugs when accessing api

---
+++ README.md
@@ -0,0 +1,21 @@
+# Weather App in ReactJS
+
+## Features and description: 
+  This app Searches for the weather information of a given city and changes the background according to the actual temperature in Celcius. It accesses the openweathermap.org api and display the information accordingly. 
+
+
+## Technologies used:
+* React JS 
+* React JS state manipulation
+*  Third party Weather API [Open Weather Map](https://openweathermap.org/)
+
+## How to install it: 
+1. git pull https://github.com/codeecoffee/WeatherApp-React.git
+2. yarn init
+
+## Soon to be implemented: 
+1. Background will change to the city being searched for 
+2. Rain, snow, and sunny effects on top of background
+3. Dynamic search bar positioning after search
+4. Search bar dropdown for specifiying the exact city. 
+5. Temperature changes from C to F on click. 
--- src/App.js
+++ src/App.js
@@ -1,45 +1,64 @@
-import React from 'react';
-//import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
 const api = {
   key: "5967f6b3a6c592153a87c49b57243ecc",
   base: "https://api.openweathermap.org/data/2.5/"
 }
-function App() {
-  const dateBuilder = (d) => {
-    let months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-    let day = days [d.getDay()]
-    let date = d.getDate()
-    let month = months[d.getMonth()]
-    let year = d.getFullYear()
+function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
 
     return `${day} ${date} ${month} ${year}`
   }
+
   return (
-    <div className="app">
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
         <div className="search-box">
           <input 
             type="text"
             className="search-bar"
-            placeholder="Searching..."  
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
           />
         </div>
+        {(typeof weather.main != "undefined") ? (
         <div>
           <div className="location-box">
-            <div className="location">Any city, US</div>
+            <div className="location">{weather.name}, {weather.sys.country}</div>
             <div className="date">{dateBuilder(new Date())}</div>
           </div>
           <div className="weather-box">
             <div className="temp">
-              15 C
+              {Math.round(weather.main.temp)}°c
             </div>
-            <div className="weather">Sunny</div>
+            <div className="weather">{weather.weather[0].main}</div>
           </div>
         </div>
+        ) : ('')}
       </main>
     </div>
   );
